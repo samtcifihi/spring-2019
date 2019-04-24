fe85310fbfc2c477348ede6c8dc8f1046a25a974
samtcifihi <la.lojban@gmail.com>
2019-04-24 10:02:56 -0400

    Changes

---
+++ CS 3500/lab8func.js
@@ -0,0 +1,22 @@
+//alert("How are you??")
+
+var myAge = 30;
+var firstName = "Joshua";
+var lastName = "Taylor";
+var isMarried = false;
+
+var myArray = [10, 15, 25];
+var Array1 = myArray;
+Array1[0]="Apple";
+
+console.log(array1[0]);
+console.log(myArray[0])
+
+var person = {
+	lastName: "Smith",
+	firstName: "Cody",
+	age: 34,
+	isMarried: false,
+}
+
+console.log("Hello " + person.lastName);
